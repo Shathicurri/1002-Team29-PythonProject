8378a483653dea29d9a9586d6629558e8a351688
Shathiya <53213780+Shathicurri@users.noreply.github.com>
2021-10-27 21:10:33 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 # initalise the tkinter GUI
 root = tk.Tk()
 
-root.title("Digital Crime Analyzer")
+root.title("Digital Crime Analyzer") # title of application
 root.geometry("700x700") # set the root dimensions
 root.pack_propagate(False) # tells the root to not let the widgets inside it determine its size.
 root.resizable(0, 0) # makes the root window fixed in size.
 
-# Set the tabs in GUI
+# Setting the tabs in GUI
 tabcontrol = ttk.Notebook(root)
 tab1 = ttk.Frame(tabcontrol)
 tab2 = ttk.Frame(tabcontrol)
@@ -62,7 +62,6 @@ buttonDonut = tk.Button(frame2, text="Donut Chart", command=lambda: Donut_chart(
 buttonDonut.place(rely=0.30, relx=0.70)
 
 # Take in values entered for Search function
-# Entry
 label_dst = tk.Label(file_frame, text="Dst Port")
 label_dst.place(rely=0.54, relx=0.10)
 
@@ -312,11 +311,13 @@ def Prediction():
     df = pd.read_csv(label_file["text"])
     data = pd.DataFrame(df)
     timestamp = data.pop('Timestamp')
-
+    
+    # Run the current data through the model to get the predictions
     model = load_model("model/training_2.h5")
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     predictions = model.predict(data.values)
-
+    
+    # combine the dataset to display
     for p1, p2, p3, t in zip(predictions, df['Protocol'].values, df['Dst Port'].values, timestamp):
         small_output = []
         small_output.append(atks[argmax(p1)])
@@ -326,7 +327,8 @@ def Prediction():
         output.append(small_output)
 
     combination1 = pd.DataFrame(np.array(output))
-
+    
+    # Display the dataframe made above
     tv2["column"] = ['Prediction', 'Protocol', 'Dst Port', 'Timestamp']
     tv2["show"] = "headings"
     for column in tv2["columns"]:
